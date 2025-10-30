b83629949198643dac9c572760452ad8a2d33888
Aby Leyva <abyleyva75@gmail.com>
2025-10-30 17:46:02 -0600

    New commit repo csharp core to learn

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.ReadLine();
+Console.WriteLine("Learning C# fundamentals by AbyLeyva!!!");
+Console.WriteLine("Press Enter to exit...");
+Console.ReadLine();
