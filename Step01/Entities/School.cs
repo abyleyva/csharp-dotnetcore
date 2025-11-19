7c1fd05abc97788c8fa913d3322eaf92857f1d1d
Aby Leyva <abyleyva75@gmail.com>
2025-11-19 17:02:03 -0600

    Avances clase 16 , Fundamentos NetCore 2.1

---
+++ Step01/Entities/School.cs
@@ -0,0 +1,44 @@
+using System.Security.Authentication;
+
+namespace CoreSchool.Entities
+{
+    class School
+    {
+        string name;
+        public string Name
+        {
+            get { return name; }
+            set {name=value.ToUpper(); }
+        }
+
+        public int YearOfFoundation { get; set; }
+
+        public string Country { get; set; }
+        public string City { get; set; }
+        
+        /* public School(string name,int yearOfFoundation)
+        {
+            this.name = name.ToUpper();
+            this.YearOfFoundation = yearOfFoundation;
+        }
+ */
+        public SchoolTypes SchoolType { get; set; }
+
+        
+
+        public School(string name,int year)=>(Name, YearOfFoundation)=(name.ToUpper(), year);
+
+        public School(string name,int year, SchoolTypes schoolType,string country="",string city="")
+        {
+            (Name,YearOfFoundation)=(name.ToUpper(), year);
+            Country=country;
+            City=city;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Year: {YearOfFoundation}, Country: {Country}, City: {City}, {System.Environment.NewLine} SchoolType: {SchoolType}";
+        }
+    }
+
+}
--- Step01/Program.cs
+++ Step01/Program.cs
@@ -1,2 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using CoreSchool.Entities;
+
+var school = new School("test school aby", 2000, SchoolTypes.Primary,country:"Colombia",city:"Bogota");
+
+var Course1 = new Course()
+{
+    Name="101",
+    JournalType=JournalTypes.Partial
+};
+var Course2 = new Course()
+{
+    Name="201",
+    JournalType=JournalTypes.Final
+};
+var Course3 = new Course()
+{
+    Name="301",
+    JournalType=JournalTypes.Extraordinary
+};
+
+//school.Country = "USA";
+//school.City = "New York";
+//school.SchoolType=SchoolTypes.Secondary;
+Console.WriteLine(school.Name);
+Console.WriteLine(school);
+System.Console.WriteLine("++++++++++=++++++++++");
+Console.WriteLine($"Course1: {Course1.Name}, JournalType: {Course1.JournalType} Id: {Course1.UniqueId}");
+Console.WriteLine($"Course2: {Course2.Name}, JournalType: {Course2.JournalType} Id: {Course2.UniqueId}");
+Console.WriteLine($"Course3: {Course3.Name}, JournalType: {Course3.JournalType} Id: {Course3.UniqueId}");
